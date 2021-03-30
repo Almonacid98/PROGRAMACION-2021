747252bd748353f91c904e764157da7c9f609415
Almonacid98 <you@example.com>
2021-03-30 20:13:36 -0300

    Se modifico un poco __init__.py dentro de la carpeta main(FALTA TERMINAR)

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -1,8 +1,18 @@
 import os 
 from flask import Flask
 from dotenv import load_dotenv
+from flask_restful import Api
+import main.resources as resources 
+
+api = Api()
 
 def create_app():
     app = Flask(__name__)
     load_dotenv()
+
+
+
+
+
+    api.init_app(app)
     return app
