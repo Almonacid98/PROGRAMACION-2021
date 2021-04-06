0c86249c312defaa695da32e1e040208c77db0ae
Almonacid98 <you@example.com>
2021-04-06 13:08:28 -0300

    Se agrego Compra.py con sus clases y métodos

---
+++ backend/main/resources/Compra.py
@@ -0,0 +1,25 @@
+from flask_restful import Resource
+from flask import request
+
+
+
+
+
+class Compra():
+
+    def get(self, id):
+
+
+    def delete(self, id):
+
+
+    def put(self, id):
+
+
+class Compras():
+
+    def get(self):
+
+
+    def post(self):
+
