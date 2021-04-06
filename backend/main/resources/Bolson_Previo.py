4bd1a20937bfbf2793641e01a0fad26253690590
Almonacid98 <you@example.com>
2021-04-06 20:24:10 -0300

    Se completo Bolson_Previo

--- backend/main/resources/Bolson_Previo.py
+++ backend/main/resources/Bolson_Previo.py
@@ -7,11 +7,12 @@ from flask import request
 class BolsonPrevio():
 
     def get(self, id):
-
-
-
+        if int(id) in BOLSONES:
+            return BOLSONES[int(id)]
+        return '', 404
 
 
 class BolsonesPrevios():
 
     def get(self):
+        return BOLSONES
