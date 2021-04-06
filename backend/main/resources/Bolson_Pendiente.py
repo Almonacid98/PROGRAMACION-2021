96920bf9371219f3dfe224a1075b1bdf6b168523
Almonacid98 <you@example.com>
2021-04-06 20:33:46 -0300

    Se completaron los metodos de Bolson_Pendiente funcionando

--- backend/main/resources/Bolson_Pendiente.py
+++ backend/main/resources/Bolson_Pendiente.py
@@ -9,18 +9,32 @@ from flask import request
 class BolsonPendiente():
 
     def get(self, id):
-
+        if int(id) in BOLSONES:
+            return BOLSONES[int(id)]
+        return '', 404
 
 
     def delete(self, id):
-
+        if int(id) in BOLSONES:
+            del BOLSONES[int(id)]
+            return '', 204
+        return '', 404
 
     def put(self, id):
-
+        if int(id) in BOLSONES:
+            bolson = BOLSONES[int(id)]
+            data = request.get_json()
+            bolson.update(data)
+            return bolson, 201
+        return '', 404
 
 class BolsonesPendientes():
 
     def get(self):
-
+        return BOLSONES
 
     def post(self):
+        bolson = request.get_json()
+        id = int(max(BOLSONES.keys())) + 1
+        BOLSONES[id] = bolson
+        return BOLSONES[id], 201
