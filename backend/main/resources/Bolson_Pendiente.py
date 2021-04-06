7d10c21c4df1a49d15a10314338961d72d4b65ca
Almonacid98 <you@example.com>
2021-04-06 13:28:06 -0300

    Renombramiento de archivos

---
+++ backend/main/resources/Bolson_Pendiente.py
@@ -0,0 +1,26 @@
+from flask_restful import Resource
+from flask import request 
+
+
+
+
+
+
+class BolsonPendiente():
+
+    def get(self, id):
+
+
+
+    def delete(self, id):
+
+
+    def put(self, id):
+
+
+class BolsonesPendientes():
+
+    def get(self):
+
+
+    def post(self):
---
+++ backend/main/resources/Bolson_Venta.py
@@ -0,0 +1,17 @@
+from flask_restful import Resource
+from flask import request
+
+
+
+
+
+class BolsonVenta():
+
+    def get(self, id):
+
+
+
+
+class BolsonesVenta():
+
+    def get(self):
