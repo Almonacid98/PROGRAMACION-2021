9b6202ccc83862a1e941b22600122d8692c04468
Almonacid98 <you@example.com>
2021-04-06 14:00:57 -0300

    Cambio de nombre de la importación

---
+++ backend/main/resources/__init__.py
@@ -0,0 +1,23 @@
+from .Bolson import Bolson as BolsonResource
+from .Bolson import Bolsones as BolsonesResource
+
+from .Bolson_Venta import BolsonVenta as BolsonVentaResource
+from .Bolson_Venta import BolsonesVenta as BolsonesVentaResource
+
+from .Bolson_Pendiente import BolsonPendiente as BolsonPendienteResource
+from .Bolson_Pendiente import BolsonesPendientes as BolsonesPendientesResource
+
+from .Bolson_Previo import BolsonPrevio as BolsonPrevioResource
+from .Bolson_Previo import BolsonesPrevios as BolsonesPreviosResource
+
+from .Producto import Producto as ProductoResource 
+from .Producto import Productos as ProductosResource
+
+from .Compra import Compra as CompraResource
+from .Compra import Compras as ComprasResource 
+
+from .Cliente import Cliente as ClienteResource
+from .Cliente import Clientes as ClientesResource
+
+from .Proveedor import Proveedor as ProveedorResource
+from .Proveedor import Proveedores as ProveedoresResource
