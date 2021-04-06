2e3000cf45624bfc886edadae3ccc01641edc4a4
Almonacid98 <you@example.com>
2021-04-06 13:05:09 -0300

    Se agrego Producto.py con sus clases y métodos

---
+++ backend/main/resources/Producto.py
@@ -0,0 +1,24 @@
+from flask_restful import Resource
+from flask import request
+
+
+
+
+class Producto():
+
+    def get(self, id):
+
+
+    def delete(self, id):
+
+
+    def put(self, id):
+
+
+class Productos():
+
+    def get(self):
+
+
+    def post(self):
+
