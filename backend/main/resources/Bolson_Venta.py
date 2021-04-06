0eff8e734154fc85f4833e243bd683b93c34c8bb
Almonacid98 <you@example.com>
2021-04-06 20:17:29 -0300

    Se agregaron métodos

--- backend/main/resources/Bolson_Venta.py
+++ backend/main/resources/Bolson_Venta.py
@@ -8,10 +8,12 @@ from flask import request
 class BolsonVenta():
 
     def get(self, id):
-
-
+        if int(id) in BOLSONES:
+            return BOLSONES[int(id)]
+        return '', 404
 
 
 class BolsonesVenta():
 
     def get(self):
+        return BOLSONES
