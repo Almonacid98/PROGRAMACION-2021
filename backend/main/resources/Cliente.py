6e81db27e2a4328afa3c6fb68eb5f721d38727b8
Almonacid98 <you@example.com>
2021-04-06 13:12:49 -0300

    Se agrego Cliente.py y Proveedores.py con sus respectivas clases y métodos

---
+++ backend/main/resources/Cliente.py
@@ -0,0 +1,25 @@
+from flask_restful import Resource
+from flask import request
+
+
+
+
+
+class Cliente():
+
+    def get(self, id):
+
+
+    def delete(self, id):
+
+
+    def put(self, id):
+
+
+class Clientes():
+
+    def get(self):
+
+
+    def post(self):
+
---
+++ backend/main/resources/Proveedores.py
@@ -0,0 +1,25 @@
+from flask_restful import Resource
+from flask import request
+
+
+
+
+
+class Proveedor():
+
+    def get(self, id):
+
+
+    def delete(self, id):
+
+
+    def put(self, id):
+
+
+class Proveedores():
+
+    def get(self):
+
+
+    def post(self):
+
