976f9506f3befe678da3c3d3c05abee30dbb050e
Almonacid98 <you@example.com>
2021-04-08 09:46:08 -0300

    Se agregaron nuevos métodos a Clientes.py

--- backend/main/resources/Cliente.py
+++ backend/main/resources/Cliente.py
@@ -8,18 +8,32 @@ from flask import request
 class Cliente():
 
     def get(self, id):
-
+        if int(id) in CLIENTES:
+            return CLIENTES[int(id)]
+        return '', 404
 
     def delete(self, id):
-
+        if int(id) in CLIENTES:
+            del CLIENTES[int(id)]
+            return '', 204
+        return '', 404
 
     def put(self, id):
-
+        if int(id) in CLIENTES:
+            cliente = CLIENTES[int(id)]
+            data = request.get_json()
+            cliente.update(data)
+            return cliente, 201
+        return '', 404
 
 class Clientes():
 
     def get(self):
-
+        return CLIENTES
 
     def post(self):
+        cliente = request.get_json()
+        id = int(max(CLIENTES.keys())) + 1
+        CLIENTES[id] = cliente
+        return CLIENTES[id], 201
 
