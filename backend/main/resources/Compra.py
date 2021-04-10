80e659cc218d189d8f5a38787025ef766e64a172
Almonacid98 <you@example.com>
2021-04-10 19:58:27 -0300

    Se modifico Compra.py, se agregaron métodos

--- backend/main/resources/Compra.py
+++ backend/main/resources/Compra.py
@@ -8,18 +8,32 @@ from flask import request
 class Compra():
 
     def get(self, id):
-
+        if int(id) in COMPRAS:
+            return COMPRAS[int(id)]
+        return '', 404
 
     def delete(self, id):
-
+        if int(id) in COMPRAS:
+            del COMPRAS[int(id)]
+            return '', 204
+        return '', 404
 
     def put(self, id):
-
+         if int(id) in COMPRAS:
+            compra = COMPRAS[int(id)]
+            data = request.get_json()
+            compra.update(data)
+            return compra, 201
+         return '', 404
 
 class Compras():
 
     def get(self):
-
+        return COMPRAS
 
     def post(self):
+        compra = request.get_json()
+        id = int(max(COMPRAS.keys())) + 1
+        COMPRAS[id] = compra
+        return COMPRAS[id], 201
 
