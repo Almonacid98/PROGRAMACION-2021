6199131c256cee09d61830b20c969121f4775016
Almonacid98 <you@example.com>
2021-04-10 19:53:47 -0300

    Se agregaron los métodos a Productos.py

--- backend/main/resources/Producto.py
+++ backend/main/resources/Producto.py
@@ -7,18 +7,31 @@ from flask import request
 class Producto():
 
     def get(self, id):
-
+        if int(id) in PRODUCTOS:
+            return PRODUCTOS[int(id)]
+        return '', 404
 
     def delete(self, id):
-
+        if int(id) in PRODUCTOS:
+            del PRODUCTOS[int(id)]
+            return '', 204
+        return '', 404
 
     def put(self, id):
-
+        if int(id) in PRODUCTOS:
+            producto = PRODUCTOS[int(id)]
+            data = request.get_json()
+            producto.update(data)
+            return producto, 201
+        return '', 404
 
 class Productos():
 
     def get(self):
-
+        return PRODUCTOS
 
     def post(self):
-
+        producto = request.get_json()
+        id = int(max(PRODUCTOS.keys())) + 1
+        PRODUCTOS[id] = producto
+        return PRODUCTOS[id], 201
