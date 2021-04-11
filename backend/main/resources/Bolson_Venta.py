576c7e0c6851f3425a7dc8f56c01bf73f4bd84cf
Gabriel <almonacid98>
2021-04-11 00:54:07 -0300

    Arregle Bolson_Venta, ahora si reconoce atributo

--- backend/main/resources/Bolson_Venta.py
+++ backend/main/resources/Bolson_Venta.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
-from flask import request
 
+
+#Pr�ximo diccionario
 BOLSONES = {
     1: {'name' : 'Bolson A'},
     2: {'name' : 'Bolson B'},
@@ -11,7 +12,9 @@ BOLSONES = {
     }
 
 
-class BolsonVenta():
+
+#Recursos de Bols�n"
+class BolsonVenta(Resource):
 
     def get(self, id):
         if int(id) in BOLSONES:
@@ -19,7 +22,7 @@ class BolsonVenta():
         return '', 404
 
 
-class BolsonesVenta():
+class BolsonesVenta(Resource):
 
     def get(self):
         return BOLSONES
