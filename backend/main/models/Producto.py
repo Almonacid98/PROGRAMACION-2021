aa3770d74aba35a702ed470ba0cbfac2b2644032
Gabriel <almonacid98>
2021-04-15 18:56:24 -0300

    Se agrego Producto.py + Código a models

---
+++ backend/main/models/Producto.py
@@ -0,0 +1,25 @@
+from .. import db
+
+class Producto(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    proveedorid = db.Column(db.Integer, db.ForeignKey('proveedor.id', ondelete="RESTRICT"), nullable=False))
+    nombre = db.Column(db.String(100), nullable = False)
+
+    def __repr__(self):
+        return '<Producto: %r >' % (self.nombre)
+
+    def to_json(self):
+        producto_json = {
+            'id' : self.id,
+            'nombre' : str(self.nombre),
+
+        }
+        return producto_json
+    @staticmethod
+
+    def from_json(producto_json):
+        id = producto_json.get('id')
+        nombre = producto_json.get('nombre')
+        return Producto(id = id,
+                        nombre = nombre, 
+                            )
