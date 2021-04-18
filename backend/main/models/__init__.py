444a359ec35c84f2619f1ece29df0d207c391bbe
Almonacid98 <Almonacid98>
2021-04-18 00:02:52 -0300

    Se agregaron las importaciones requeridas en __init__.py de models

--- backend/main/models/__init__.py
+++ backend/main/models/__init__.py
@@ -1,2 +1,5 @@
 from .Bolson import Bolson as BolsonModel
+from .Producto import Producto as ProductoModel
+from .Compra import Compra as CompraModel
 from .Cliente import Cliente as ClienteModel
+from .Proveedor import Proveedor as ProveedorModel
