18809abbfc0126f5f1359486b089cb7e0ebf4473
Almonacid98 <Almonacid98>
2021-04-20 19:44:29 -0300

    Se creo archivo Producto_Bolson.py con sus relaciones, clases y métodos

---
+++ backend/main/models/Producto_Bolson.py
@@ -0,0 +1,31 @@
+from .. import db
+from main.models import ProductoModel
+from main.models import BolsonModel
+
+class ProductoBolson(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    productoid = db.Column(db.Integer, db.ForeignKey('producto.id'), nullable = False)
+    bolsonid = db.Column(db.Integer, db.ForeignKey('bolson.id'), nullable = False)
+    producto = db.relationship('Producto', back_populates = 'productos_bolsones', uselist = False, single_parent = True)
+    bolson = db.relationship('Bolson', back_populates = 'productos_bolsones', uselist = False, single_parent = True)
+
+    def to_json(self):
+        self.producto = db.session.query(ProductoModel).get_or_404(self.productoid)
+        self.bolson = db.session.query(BolsonModel).get_or_404(self.bolsonid)
+        productobolson_json = {
+            'id' : self.id,
+            'producto' : self.producto.to_json(),
+            'bolson' : self.bolson.to_json(),
+        }
+        return productobolson_json
+    @staticmethod
+
+    def from_json(productobolson_json):
+        id = productobolson_json.get('id')
+        productoid = productobolson_json.get('productoid')
+        bolsonid = productobolson_json.get('bolsonid')
+        return ProductoBolson(id = id,
+                            productoid = productoid,
+                            bolsonid = bolsonid,
+                            )
