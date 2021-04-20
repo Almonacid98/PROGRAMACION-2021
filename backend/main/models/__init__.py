f551901b4730b0c81ce438e9b90712ca412ba121
Almonacid98 <Almonacid98>
2021-04-20 20:04:43 -0300

    Se importó archivo Producto_Bolson.py como ProductoBolsonModel

--- backend/main/models/__init__.py
+++ backend/main/models/__init__.py
@@ -3,3 +3,4 @@ from .Producto import Producto as ProductoModel
 from .Compra import Compra as CompraModel
 from .Cliente import Cliente as ClienteModel
 from .Proveedor import Proveedor as ProveedorModel
+from .Producto_Bolson import ProductoBolson as ProductoBolsonModel
