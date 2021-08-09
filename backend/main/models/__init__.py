67e613347090bb0eaae95645abf632bdebb9e296
Almonacid98 <Almonacid98>
2021-08-09 19:05:55 -0300

    modificacion de acuerdo a cambio en las relaciones al agregar usuario.py

--- backend/main/models/__init__.py
+++ backend/main/models/__init__.py
@@ -1,7 +1,5 @@
 from .Bolson import Bolson as BolsonModel
 from .Producto import Producto as ProductoModel
 from .Compra import Compra as CompraModel
-from .Cliente import Cliente as ClienteModel
-from .Proveedor import Proveedor as ProveedorModel
 from .Producto_Bolson import ProductoBolson as ProductoBolsonModel
 from .Usuario import Usuario as UsuarioModel
