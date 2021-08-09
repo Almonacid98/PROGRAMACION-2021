f0c3700f7fe7c2f476f6929b361e3990db6cf801
Almonacid98 <Almonacid98>
2021-08-09 18:59:21 -0300

    Se agregaron relaciones con usuario en productos.py y se elimino la relacion con el archivo proveedor.py que se borro

--- backend/main/models/Producto.py
+++ backend/main/models/Producto.py
@@ -1,12 +1,12 @@
 from .. import db
-from .Proveedor import Proveedor as ProveedorModel 
+
 
 class Producto(db.Model):
     
     id = db.Column(db.Integer, primary_key = True)
     nombre = db.Column(db.String(100), nullable = False)
-    proveedorid = db.Column(db.Integer, db.ForeignKey('proveedor.id'), nullable = False)
-    proveedor = db.relationship('Proveedor', back_populates = 'productos', uselist = False, single_parent = True)
+    usuarioid = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable = False)
+    usuario = db.relationship('Usuario', back_populates = 'productos', uselist = False, single_parent = True)
     productos_bolsones = db.relationship('ProductoBolson', back_populates = 'producto')
 
     def __repr__(self):
@@ -17,7 +17,7 @@ class Producto(db.Model):
         producto_json = {
             'id' : self.id,
             'nombre' : str(self.nombre),
-            'proveedor' : self.proveedor.to_json()
+            'usuario' : self.usuario.nombre
         }
         return producto_json
     @staticmethod
@@ -25,8 +25,8 @@ class Producto(db.Model):
     def from_json(producto_json):
         id = producto_json.get('id')
         nombre = producto_json.get('nombre')
-        proveedorid = producto_json.get('proveedorid')
+        usuarioid = producto_json.get('usuarioid')
         return Producto(id = id,
                         nombre = nombre,
-                        proveedorid = proveedorid, 
+                        usuarioid = usuarioid, 
                         )
