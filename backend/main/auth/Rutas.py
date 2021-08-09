bf059e465016fdfefa91204994966fb63e23c3c0
Almonacid98 <Almonacid98>
2021-08-09 17:53:42 -0300

    Modificación de archivo Rutas.py, se agregó metodo de logeo más metodo de registro

---
+++ backend/main/auth/Rutas.py
@@ -0,0 +1,42 @@
+from os import access
+from sqlalchemy.sql.base import Executable
+
+from sqlalchemy.sql.operators import exists
+from backend.main.models.Usuario import Usuario
+from flask import request, jsonify, Blueprint
+from .. import db
+from main.models import UsuarioModel
+from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
+
+auth = Blueprint('auth', __name__, url_prefix='/auth')
+
+@auth.route('/login', methods = ['POST'])
+
+def login():
+    usuario = db.session.query(UsuarioModel).filter(UsuarioModel.email == request.get_json().get('email')).first_or_404()
+    if usuario.validate_pass(request.get_json().get('contraseña')):
+        access_token = create_access_token(identity= usuario)
+        data = {
+            'id' : str(usuario.id),
+            'email' : usuario.email,
+            'access_token' : access_token
+        }
+        return data, 200
+    else:
+        return 'Contraseña incorrecta', 401
+
+@auth.route('/register', methods = ['POST'])
+
+def register():
+    usuario = UsuarioModel.from_json(request.get_json())
+    exists = db.session.query(UsuarioModel).filter(UsuarioModel.email == usuario.email).scalar() is not None
+    if exists:
+        return 'Email duplicado', 409
+    else:
+        try:
+            db.session.add(usuario)
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            return str(error), 409
+        return usuario.to_json(), 201
