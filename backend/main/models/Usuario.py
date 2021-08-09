32d0aa55b62f1a0af8cc29b1ec83c40dfb9dadfa
Almonacid98 <Almonacid98>
2021-08-09 16:38:14 -0300

    Se agregaron relaciones con compra y productos mas datos de tablas para armar

--- backend/main/models/Usuario.py
+++ backend/main/models/Usuario.py
@@ -1,1 +1,16 @@
+from enum import unique
+from operator import index
 from .. import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(100), nullable = False)
+    apellido = db.Column(db.String(100), nullable = False)
+    telefono = db.Column(db.String(100), nullable = False)
+    email = db.Column(db.String(64), unique = True, index = True, nullable = False)
+    contraseña = db.Column(db.String(128), nullable = False)
+    rol = db.Column(db.String(10), nullable = False, default = 'user')
+    compras = db.relationship('Compra', back_populate = 'usuarios', cascade = 'all, delete-orphan')
+    productos = db.relationship('Producto', back_populates = 'usuarios')
+
