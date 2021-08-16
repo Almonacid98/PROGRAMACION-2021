4305592c61ff520b0cfef428476ea70898ef6159
Almonacid98 <Almonacid98>
2021-08-16 04:13:06 -0300

    Se modifico por defaul a cliente

--- backend/main/models/Usuario.py
+++ backend/main/models/Usuario.py
@@ -4,13 +4,14 @@ from .. import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class Usuario(db.Model):
+
     id = db.Column(db.Integer, primary_key = True)
     nombre = db.Column(db.String(100), nullable = False)
     apellido = db.Column(db.String(100), nullable = False)
     telefono = db.Column(db.String(100), nullable = False)
-    email = db.Column(db.String(64), unique = True, index = True, nullable = False)
-    password = db.Column(db.String(128), nullable = False)
-    rol = db.Column(db.String(10), nullable = False, default = 'user')
+    email = db.Column(db.String(64), unique=True, index=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    rol = db.Column(db.String(10), nullable = False, default = 'cliente')
     compras = db.relationship('Compra', back_populates = 'usuarios', cascade = 'all, delete-orphan')
     productos = db.relationship('Producto', back_populates = 'usuarios')
 
@@ -18,11 +19,10 @@ class Usuario(db.Model):
     def plain_password(self):
         raise AttributeError('La contraseña no se puede leer')
     @plain_password.setter
-    
     def plain_password(self, password):
         self.password = generate_password_hash(password)
     
-    def validate_pass(self, password):
+    def validate_pass(self,password):
         return check_password_hash(self.password, password)
     
     def __repr__(self):
@@ -34,7 +34,7 @@ class Usuario(db.Model):
             'nombre' : str(self.nombre),
             'apellido' : str(self.apellido),
             'telefono' : str(self.telefono),
-            'email' : str(self.email)
+            'email' : str(self.email),
 
         }
         return usuario_json
@@ -57,3 +57,6 @@ class Usuario(db.Model):
                     plain_password = password,
                     rol = rol,
                     )
+    """@classmethod
+    def changed_password(self, db, usuario):
+        self.password_hashed = Usuario.generate_password_hash(password)"""
